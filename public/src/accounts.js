599c4a3e965338ee67fe80e019f1e7ca9162b5ae
lenar-indeed <145393744+lenar-indeed@users.noreply.github.com>
2023-09-27 09:56:59 -0500

    Complete qualified assignment

---
+++ public/src/accounts.js
@@ -0,0 +1,46 @@
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+ }
+
+function sortAccountsByLastName(accounts) {
+  accounts.sort((a, b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1);
+  return accounts;
+}
+
+function getAccountFullNames(accounts) {
+  return accounts.map(account => {
+    const {name: {first, last} } = account;
+    return `${first} ${last}`;
+  });
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((acc, book) => {
+    const count = book.borrows.reduce((borrowAcc, borrow) => {
+      return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
+    }, 0);
+
+    return acc + count;
+  }, 0);
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  return books
+    .filter((book) => {
+      const recent = book.borrows[0];
+      return !recent.returned && recent.id === account.id;
+    })
+    .map((book) => {
+      const author = authors.find((author) => author.id === book.authorId);
+      return { ...book, author };
+    });
+}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  getAccountFullNames,
+  getTotalNumberOfBorrows,
+  getBooksPossessedByAccount,
+};
